cbbce6a409f4c7c3c891b37203e4a39b833592dc
brandon9598 <brandon9598@gmail.com>
2022-03-23 21:24:25 -0400

    Created main swap compoment with styles

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,5 @@
 import Header from '../components/Header'
+import Main from '../components/Main'
 
 const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
@@ -8,7 +9,7 @@ const Home = () => {
   return (
     <div className={style.wrapper}>
       <Header />
-      <h2>Main</h2>
+      <Main />
       <h2>Transaction Header</h2>
     </div>
   )
