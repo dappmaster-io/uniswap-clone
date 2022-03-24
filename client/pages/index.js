ec2a9547c676c8d76ed9d5135a78464a9d4c791e
brandon9598 <brandon9598@gmail.com>
2022-03-23 23:00:52 -0400

    Add in transaction history from sanity DB

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,6 @@
 import Header from '../components/Header'
 import Main from '../components/Main'
+import TransactionHistory from '../components/TransactionHistory'
 
 const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
@@ -10,7 +11,7 @@ const Home = () => {
     <div className={style.wrapper}>
       <Header />
       <Main />
-      <h2>Transaction Header</h2>
+      <TransactionHistory />
     </div>
   )
 }
